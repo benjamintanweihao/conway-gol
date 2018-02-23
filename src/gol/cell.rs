b411cdbcc74ca180589d9b1f97bc1fea10c04111
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2018-02-23 10:00:46 +0800

    Implement liveness in Cells

--- src/gol/cell.rs
+++ src/gol/cell.rs
@@ -1,12 +1,21 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Cell {
     x: i32,
     y: i32,
+    alive: bool,
 }
 
 impl Cell {
-    pub fn new(x: i32, y: i32) -> Cell {
-        Cell { x: x, y: y }
+    pub fn new(x: i32, y: i32, alive: bool) -> Cell {
+        Cell {
+            x: x,
+            y: y,
+            alive: alive,
+        }
+    }
+
+    pub fn alive(&self) -> bool {
+        self.alive
     }
 }
 
@@ -16,9 +25,39 @@ mod tests {
 
     #[test]
     fn new_cell() {
-        let cell = Cell::new(-4, 2);
+        let cell = Cell::new(-4, 2, true);
 
         assert_eq!(-4, cell.x);
         assert_eq!(2, cell.y);
+        assert_eq!(true, cell.alive);
+    }
+
+    #[test]
+    fn equality() {
+        let first = Cell::new(-4, 2, true);
+        let second = Cell::new(-4, 2, true);
+
+        assert_eq!(first, first);
+        assert_eq!(first, second);
+        assert_eq!(second, first);
+    }
+
+    #[test]
+    fn inequality() {
+        let first = Cell::new(2, -4, true);
+        let second = Cell::new(-4, 2, true);
+        let third = Cell::new(-4, 2, false);
+
+        assert_ne!(first, second);
+        assert_ne!(second, third);
+    }
+
+    #[test]
+    fn liveness() {
+        let alive = Cell::new(0, 0, true);
+        let dead = Cell::new(0, 0, false);
+
+        assert!(alive.alive());
+        assert!(!dead.alive());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod gol;
 use gol::cell::Cell;
 
 fn main() {
-    let cell = Cell::new(0, 0);
+    let cell = Cell::new(0, 0, true);
 
     println!("The cell is at {:#?}", cell);
 }
