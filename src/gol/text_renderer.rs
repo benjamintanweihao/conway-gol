e23eb4f3475e813d3ec0a2acbb05cf3efc78ef22
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2018-02-24 12:28:32 +0800

    GL renderer!
    Everything is in main.rs. I will extract this in the next commit.

--- src/gol/text_renderer.rs
+++ src/gol/text_renderer.rs
@@ -1,3 +1,4 @@
+#[allow(dead_code)]
 pub mod text_renderer {
     use gol::world::{World, SIZE};
 
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ extern crate sdl2;
 mod gol;
 
 use std::{thread, time};
-use gol::world::{Position, World};
-use gol::text_renderer::text_renderer;
+use gol::world::{Position, World, SIZE};
 
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
+
+const CELL_SIZE: u32 = 4;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -27,14 +27,9 @@ fn main() {
     canvas.clear();
     canvas.present();
 
-    let cell_color = Color::RGB(0, 255, 0);
-    canvas.set_draw_color(cell_color);
-    canvas.fill_rect(Rect::new(10, 10, 10, 10));
-    canvas.fill_rect(Rect::new(0, 0, 10, 10));
-    canvas.fill_rect(Rect::new(20, 20, 10, 10));
-    canvas.present();
-
     let mut event_pump = sdl_context.event_pump().unwrap();
+    let duration = time::Duration::from_millis(50);
+    let mut world = World::new(combination());
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -47,19 +42,34 @@ fn main() {
                 _ => {}
             }
         }
+
+        world = world.tick();
+
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+
+        let cell_color = Color::RGB(0, 255, 0);
+        canvas.set_draw_color(cell_color);
+
+        for y in 0..SIZE {
+            for x in 0..SIZE {
+                if world.is_alive((x, y)) == true {
+                    let _ = canvas.fill_rect(Rect::new(
+                        (x as i32) * (CELL_SIZE as i32),
+                        (y as i32) * (CELL_SIZE as i32),
+                        CELL_SIZE - 1,
+                        CELL_SIZE - 1,
+                    ));
+                }
+            }
+        }
+
+        canvas.present();
+
+        thread::sleep(duration);
     }
-
-    // let world = World::new(combination());
-
-    // let duration = time::Duration::from_millis(100);
-    // let mut world = world;
-    // loop {
-    //     text_renderer::render(&world);
-    //     world = world.tick();
-    //     thread::sleep(duration);
-    // }
 }
-
+#[allow(dead_code)]
 fn combination() -> Vec<Position> {
     vec![
         (0, 12),
@@ -73,6 +83,7 @@ fn combination() -> Vec<Position> {
     ]
 }
 
+#[allow(dead_code)]
 fn glider() -> Vec<Position> {
     vec![(1, 0), (2, 1), (0, 2), (1, 2), (2, 2)]
 }
