270ac1c2e069636782bd23cdf4484172cf66c606
Benjamin Tan Wei Hao <benjamin.tan@easymile.com>
2018-02-26 17:40:26 +0800

    Spaceship!

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ use gol::gl_renderer::gl_renderer;
 use gol::rle_reader::rle_reader;
 
 fn main() {
-    let size = 1714;
-    let positions = rle_reader::read("./res/tm.lif");
+    let size = 300;
+    let positions = rle_reader::read("./res/c5greyship.lif");
     let world = World::new(positions, size);
     gl_renderer::render(&world);
 }
