ca46a991576a489ebf1046a91810cad610d43c9a
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2018-02-28 07:55:40 +0800

    Implement command line args
    Conway's Game of Life
    
    USAGE:
        conway-gol -f <file> -u <url>
    
    FLAGS:
        -h, --help       Prints help information
        -V, --version    Prints version information
    
    OPTIONS:
        -f <file>        the file path of the RLE file
        -u <url>         the URL of the RLE file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod gol;
 
+extern crate clap;
 extern crate reqwest;
 extern crate sdl2;
 
@@ -8,18 +9,48 @@ use std::fs::File;
 use gol::world::World;
 use gol::gl_renderer::gl_renderer;
 use gol::rle_reader::rle_reader;
+use clap::{App, Arg};
 
 fn main() {
-    // let path = "./res/tm.rle";
-    // let f = File::open(path).expect("file not found");
-    // let file = BufReader::new(&f);
+    let matches = App::new("Conway's Game of Life")
+        .arg(
+            Arg::with_name("file")
+                .help("the file path of the RLE file")
+                .takes_value(true)
+                .short("f")
+                .multiple(false)
+                .required(true)
+                .conflicts_with("url"),
+        )
+        .arg(
+            Arg::with_name("url")
+                .help("the URL of the RLE file")
+                .takes_value(true)
+                .short("u")
+                .multiple(false)
+                .required(true)
+                .conflicts_with("file"),
+        )
+        .get_matches();
 
-    let link = "http://www.conwaylife.com/patterns/3enginecordershiprake.rle";
-    let resp = reqwest::get(link).unwrap();
-    assert!(resp.status().is_success());
-    let file = BufReader::new(resp);
+    if matches.is_present("file") {
+        if let Some(path) = matches.value_of("file") {
+            let f = File::open(path).expect("file not found");
+            let file = BufReader::new(&f);
+            let (size, positions) = rle_reader::read(file);
+            let world = World::new(positions, size);
+            gl_renderer::render(&world);
+        }
+    }
 
-    let (size, positions) = rle_reader::read(file);
-    let world = World::new(positions, size);
-    gl_renderer::render(&world);
+    if matches.is_present("url") {
+        if let Some(url) = matches.value_of("url") {
+            let resp = reqwest::get(url).unwrap();
+            assert!(resp.status().is_success());
+            let file = BufReader::new(resp);
+            let (size, positions) = rle_reader::read(file);
+            let world = World::new(positions, size);
+            gl_renderer::render(&world);
+        }
+    }
 }
